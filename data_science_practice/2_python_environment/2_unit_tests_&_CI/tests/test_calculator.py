505203f870f1c24d71cbbcdb3f5832a421f314c5
issagith <kai658366@gmail.com>
2025-09-19 20:20:11 +0200

    new add test to test the workflow

--- data_science_practice/2_python_environment/2_unit_tests_&_CI/tests/test_calculator.py
+++ data_science_practice/2_python_environment/2_unit_tests_&_CI/tests/test_calculator.py
@@ -7,6 +7,7 @@ def test_add():
 def test_multiply():
     assert multiply(3, 4) == 12
     assert multiply(0, 5) == 0
+    assert multiply(-2, -3) == 6
 
 def test_add_edge_cases():
     assert add(0, 0) == 0
